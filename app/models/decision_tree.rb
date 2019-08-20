89628b3c3533ca577912ac9a9e467230a89d75bc
Vitalii Iurev <wwjurew@gmail.com>
2019-08-20 21:20:10 +0300

    add attr accessors and specs

--- app/models/decision_tree.rb
+++ app/models/decision_tree.rb
@@ -1,7 +1,39 @@
 class DecisionTree
   include ActiveModel::Model
 
-  attr_accessor :attribute_name # TODO: fix later
+  # `String`, `Actions`'s key
+  attr_accessor :key
+
+  <<-DOC
+    `Hash` or `[Number]`
+    All keys present all possible values for the given `key`
+    If value is `DecisionTree` – tree goes further
+    If value is array of numbers – action is classified
+    ```
+      {
+        value1: <DecisionTree>,
+        value2: <DecisionTree>,
+        value3: [1, 2]
+      }
+    ```
+  DOC
+  attr_accessor :values
+
+  <<-DOC
+    `DecisionTree` or `[Number]`,
+    alternative branch when `Action` doesn't have common `key`
+
+    If value is `DecisionTree` – it classifies action by another `key`
+    If value is array of numbers – TODO (?)
+    ```
+      default: <DecisionTree>
+    ```
+    or
+    ```
+      default: [1, 2] # TODO
+    ```
+  DOC
+  attr_accessor :default
 
   def self.construct
     {
--- spec/models/decision_tree_spec.rb
+++ spec/models/decision_tree_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe DecisionTree, type: :model do
+  describe '.new' do
+    let(:dt) do
+      DecisionTree.new({
+        key: 'color',
+        values: {
+          value1: DecisionTree.new,
+          value2: [1, 2]
+        },
+        default: DecisionTree.new,
+      })
+    end
+    it { expect(dt.key).to eq('color') }
+    it { expect(dt.values).to be_kind_of(Hash) }
+    it { expect(dt.values[:value1]).to be_kind_of(DecisionTree) }
+    it { expect(dt.values[:value2]).to be_kind_of(Array) }
+    it { expect(dt.default).to be_kind_of(DecisionTree) }
+  end
+
   describe '.construct' do
     it 'success' do
       expect(subject.class.construct).to be_kind_of(Hash)
